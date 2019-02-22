9f879a91643b4584830f4bb12929244b2e5f5cc5
absognety <vikasch.1994@gmail.com>
2019-02-22 10:51:26 +0530

    stdin, stdout and print Hello world examples in Java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by cvikas10 on 2/22/2019.
+ */
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
