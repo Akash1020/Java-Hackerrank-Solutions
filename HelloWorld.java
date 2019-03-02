2205de12790e8955cbaf9f9782c6b91c5a8cc6be
absognety <vikasch.1994@gmail.com>
2019-03-02 10:47:20 +0530

    initialize repo

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by cvikas10 on 2/22/2019.
- */
-public class HelloWorld {
-    public static void main (String[] args) {
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
-    }
-}
